565471ca639237f626108ca87856d1a482953ee6
DaniilKaletin <105595762+DaniilKaletin@users.noreply.github.com>
2025-09-14 14:11:12 +0300

    Add files via upload

---
+++ stack1.cpp
@@ -0,0 +1,13 @@
+//Создадим пустой список STACK
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main{
+	stack<int> stack;
+	for (int i = 0; i <= 13; i++){
+		mystack.puch_back(i);
+	}
+	count << mystack;
+}
